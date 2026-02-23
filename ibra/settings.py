cc17b082c2c129af197817db9b3bac76a0828e26
yurey <aitaliabdelwahed@gmail.com>
2026-02-23 14:29:12 +0000

    reer

--- ibra/settings.py
+++ ibra/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS=['https://www.brahimpparts.com']
+CSRF_TRUSTED_ORIGINS=['https://www.brahimpparts.com',
+                      'https://brahimpparts.com'
+                      ]
 CORS_ALLOWED_ORIGINS = [
     "*"
 ]
