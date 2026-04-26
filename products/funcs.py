13d2e366b0e5c4445eb1a472c7e807fb53052dec
Abdelouahed Aliali <aaliali@e1r9p4.1337.ma>
2026-04-26 19:51:22 +0100

    reer

--- products/funcs.py
+++ products/funcs.py
@@ -1,6 +1,11 @@
 import requests as req
-
-serverip = '157.245.74.156'
+import os
+usesqlite = os.getenv('DEBUG')=='True' or os.getenv('HOME')=='/home/aaliali'
+# using sqlite meqns we are in dev mode then use qnother ip server
+if usesqlite:
+    serverip = '52.25.74.15612'
+else:
+    serverip = '157.245.74.156'
 def updatestockinremoteserver(products, serverip):
     url = f"http://{serverip}/products/updatestockfromthread"
     headers = {'Content-Type': 'application/json'}
